5cb8cbf8fed83706c4575be596a2d92c0796bc4f
SaltyExorcist <57772990+SaltyExorcist@users.noreply.github.com>
2023-04-05 01:35:14 +0530

    Made significant changes in app1,py file like added a toggle b/w dark and light mode.

--- app1.py
+++ app1.py
@@ -6,12 +6,32 @@ from PIL import ImageTk, Image
 
 from model1 import predict_disease
 
+root = ttk.Window(themename="pulse")
+
+root.title("ML Project")
+#root.iconbitmap('')
+root.geometry("773x1000")
+root.minsize(620,850)
+root.maxsize(773,1000)
+
+
+#from model import predict_disease
+
 image = None
-theme="cyborg"
-m1=0
-m2=0
-m3=0
-m4=0
+cvdm=0
+pnm=0
+tbm=0
+nm=0
+
+#To Change theme
+def chgtheme():
+    global theme
+    if tb.get() == 0:
+        root.style.theme_use("pulse")
+        
+    else:
+        root.style.theme_use("cyborg")
+        
 
 #Openning required image and resizing it
 def open_image():
@@ -24,31 +44,43 @@ def open_image():
 
 #Calculating Probability
 def make_prediction():
-    global image
-    global m1,m2,m3,m4
+    global cvdm,pnm,tbm,nm
+    
     prediction = predict_disease(image)
-    m1=list(prediction.values())[0]
-    m2=list(prediction.values())[1]
-    m3=list(prediction.values())[2]
-    m4=list(prediction.values())[3]
+    cvdm=round(list(prediction.values())[0],2)
+    nm=round(list(prediction.values())[1],2)
+    pnm=round(list(prediction.values())[2],2)
+    tbm=round(list(prediction.values())[3],2)
+    covid_meter.configure(amountused=cvdm)
+    pneumonia_meter.configure(amountused=pnm)
+    tuberculosis_meter.configure(amountused=tbm)
+    normal_meter.configure(amountused=nm)
 
-def clearimage():
-    global input_button
+
+#To Reset Everything
+def clear_all():
+    global input_button,cvdm,pnm,tbm,nm
+    cvdm=0
+    pnm=0
+    tbm=0
+    nm=0
     input_button.configure(text="Select the Image",image="",padding=(152,224))
+    
+    covid_meter.configure(amountused=cvdm)
+    pneumonia_meter.configure(amountused=pnm)
+    tuberculosis_meter.configure(amountused=tbm)
+    normal_meter.configure(amountused=nm)
 
 
-root = ttk.Window(themename=theme)
-root.title("ML Project")
-#root.iconbitmap('')
-root.geometry("773x1000")
-root.minsize(620,850)
-root.maxsize(773,1000)
-
+tb = tk.IntVar()
+theme_button = ttk.Checkbutton(text="Change Theme",bootstyle="info-square-toggle",
+                               variable=tb,onvalue=1,offvalue=0,command=chgtheme)
+theme_button.grid(row=0,column=0,sticky="e")
 
 #Input Part
 #Creating Label to Seperate
 input_label = ttk.Labelframe(root,text="Input",padding=(8,8),style="success.TLabelframe")
-input_label.grid(row=0,column=0,padx=20,pady=10)
+input_label.grid(row=1,column=0,padx=20,pady=10)
 
 #Input Button
 in_style = ttk.Style()
@@ -65,36 +97,36 @@ submit_button.grid(row=1,column=0,pady=20,padx=20,sticky="w")
 #Reset Button
 res_style = ttk.Style()
 res_style.configure('danger.TButton',font=("Consolas",13))
-reset_button = ttk.Button(input_label,text="Reset",padding=(60,20),width=5,style="danger.TButton",command=clearimage)
+reset_button = ttk.Button(input_label,text="Reset",padding=(60,20),width=5,style="danger.TButton",command=clear_all)
 reset_button.grid(row=1,column=0,pady=20,padx=20,sticky="e")
 
 #Result Part
 #Creating Label to Seperate
 result_label = ttk.Labelframe(root,text="Result",padding=(8,8),style="info.TLabelframe")
-result_label.grid(row=1,column=0,padx=20,pady=10)
+result_label.grid(row=2,column=0,padx=20,pady=10)
 
 #Meter for m1
-meter1= ttk.Meter(result_label, bootstyle="success",subtext="1stResult",interactive=False,
+covid_meter= ttk.Meter(result_label, bootstyle="success",subtext="Covid",interactive=False,
                  textright="%",metertype="full",stripethickness=10,metersize=130,padding=4,
-                 amounttotal=100,subtextstyle="danger",amountused=m1)
-meter1.grid(row=0,column=0,padx=4,pady=4)
+                 amounttotal=100,subtextstyle="danger",amountused=cvdm)
+covid_meter.grid(row=0,column=0,padx=4,pady=4)
 
 #Meter for m2
-meter2= ttk.Meter(result_label, bootstyle="success",subtext="2ndResult",interactive=False,
+pneumonia_meter= ttk.Meter(result_label, bootstyle="success",subtext="Pneumonia",interactive=False,
                  textright="%",metertype="full",stripethickness=10,metersize=130,padding=4,
-                 amounttotal=100,subtextstyle="danger",amountused=m2)
-meter2.grid(row=0,column=1,padx=4,pady=4)
+                 amounttotal=100,subtextstyle="danger",amountused=pnm)
+pneumonia_meter.grid(row=0,column=1,padx=4,pady=4)
 
-#Meter for m3
-meter3= ttk.Meter(result_label, bootstyle="success",subtext="3rdResult",interactive=False,
+#Meter for m1
+tuberculosis_meter= ttk.Meter(result_label, bootstyle="success",subtext="Tubelculosis",interactive=False,
                  textright="%",metertype="full",stripethickness=10,metersize=130,padding=4,
-                 amounttotal=100,subtextstyle="danger",amountused=m3)
-meter3.grid(row=0,column=2,padx=4,pady=4)
+                 amounttotal=100,subtextstyle="danger",amountused=tbm)
+tuberculosis_meter.grid(row=0,column=2,padx=4,pady=4)
 
-#Meter for m4
-meter4= ttk.Meter(result_label, bootstyle="success",subtext="4thResult",interactive=False,
+#Meter for m1
+normal_meter= ttk.Meter(result_label, bootstyle="success",subtext="Normal",interactive=False,
                  textright="%",metertype="full",stripethickness=10,metersize=130,padding=4,
-                 amounttotal=100,subtextstyle="danger",amountused=m4)
-meter4.grid(row=0,column=3,padx=4,pady=4)
+                 amounttotal=100,subtextstyle="danger",amountused=nm)
+normal_meter.grid(row=0,column=3,padx=4,pady=4)
 
 root.mainloop();
